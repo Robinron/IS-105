34c45fe8cce96a261572a99a90decd082ea97c29
sflovik <sondrf14@uia.no>
2015-02-10 12:35:41 +0100

    Create lab_3_server.py

---
+++ lab_3_server.py
@@ -0,0 +1,40 @@
+import socket
+import sys
+ 
+HOST = 'localhost'   # Localhost
+PORT = 8888 # En ureservert port
+ 
+# Datagram (udp) socket
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+ 
+ 
+# Bind socket til local host og port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#kommuniser med client
+while 1:
+    # motta data fra client (data, addr)
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
+     
+    if not data: 
+        break
+     
+    reply = data.upper()
+     
+    s.sendto(reply , addr)
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.upper()
+     
+s.close()
