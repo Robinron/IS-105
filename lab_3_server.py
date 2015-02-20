8c66e95d830aa64a0f1fbddff7cee4a753070627
Vatne <larsolebvatne@hotmail.com>
2015-02-20 13:51:26 +0100

    Update lab_3_server.py

--- lab_3_server.py
+++ lab_3_server.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import socket
 import sys
+import lab2
  
 HOST = 'localhost'   # Localhost
 PORT = 8888 # En ureservert port
+
+
  
 # Datagram (udp) socket
 try :
@@ -23,18 +28,58 @@ except socket.error , msg:
 print 'Socket bind complete'
  
 #kommuniser med client
-while 1:
+
+def msg_to_upper(msg_in): 
+    msg_ut = [] 
+    for char in msg_in: 
+        msg_ut.append(chr(bitXor(ord(char)))) 
+    
+    return msg_ut
+        
+def bitXor(x):
+    return x^32
+    
+def test():
+    #_t = msg_to_upper('a')[0];
+    #print("test: "+_t);
+    
+    assert msg_to_upper('a')[0] == 'A'
+    #raise AssertionError()
+    return "Testen er fullført uten feil"
+print test()
+#while 1:
     # motta data fra client (data, addr)
+    #d = s.recvfrom(1024)
+    #msg = d[0]
+   # addr = d[1]
+     
+    #if not msg: 
+    #    break
+   # print msg
+   # print ' '.join(format(ord(x), 'b') for x in msg) 
+   # reply = ''.join(msg_to_upper(msg))
+    #data.decode('utf-8').upper().encode('utf-8')
+    
+   # print ' '.join(format(ord(x), 'b') for x in reply)
+   # print
+   # s.sendto(reply , addr)
+    #print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.upper()
+while 2:
     d = s.recvfrom(1024)
-    data = d[0]
+    msg = d[0]
     addr = d[1]
      
-    if not data: 
+    if not msg: 
         break
+    print msg
+    reply = lab2.roman2dec(msg)
+    msg_send = str(reply)
+    s.sendto(msg_send,addr)
+
      
-    reply = data.decode('utf-8').upper().encode('utf-8')
-     
-    s.sendto(reply , addr)
-    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.upper()
-     
+
+
+
+
 s.close()
+
