55e270bd6ad86753dd62cbca070da43f6899c8ef
Vatne <larsolebvatne@hotmail.com>
2015-05-22 17:30:17 +0200

    Update lab_3_server.py

--- Lab-3/lab_3_server.py
+++ Lab-3/lab_3_server.py
@@ -3,91 +3,64 @@
 import socket
 import sys
 import lab2
- 
- gruppe = {  'student1': 'Robin Amir Rondestvedt Moudnib', \
-            'student2': 'Ricky Løtoft Omland', \
-            'student3': 'Cuong Bui', \
-            'student4': 'Kojar Heresh Baban', \
-            'student5': 'Lars Ole Vatne', \
-            'student6': 'Sondre Flovik', \
-}
- 
-HOST = 'localhost'   # Localhost
+
+HOST = 'localhost' # Localhost
 PORT = 8888 # En ureservert port
 
-
- 
 # Datagram (udp) socket
 try :
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print 'Socket created'
+    print 'Socket opprettet'
 except socket.error, msg :
-    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print 'Feilet å lage socket. Error kode: ' + str(msg[0]) + ' Beskjed ' + msg[1]
     sys.exit()
- 
- 
+    
 # Bind socket til local host og port
 try:
     s.bind((HOST, PORT))
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print 'Bind feilet. Error kode : ' + str(msg[0]) + ' Beskjed ' + msg[1]
     sys.exit()
-     
-print 'Socket bind complete'
- 
-#kommuniser med client
-
-def msg_to_upper(msg_in): 
-    msg_ut = [] 
-    for char in msg_in: 
-        msg_ut.append(chr(bitXor(ord(char)))) 
     
+print 'Socket bind komplett'
+
+#Kommuniserer med client
+
+def msg_to_upper(msg_in):
+    msg_ut = []
+    for char in msg_in:
+        msg_ut.append(chr(bitXor(ord(char))))
     return msg_ut
-        
+#Våres funksjon for å gjøre meldingen vi mottar fra lower til uppercase. 
+  
 def bitXor(x):
     return x^32
+    #Funksjon som flipper den sjette bit'en.
+    #Dette fører til at den bokstaven den flipper går fra lower til uppercase.
     
 def test():
-    #_t = msg_to_upper('a')[0];
-    #print("test: "+_t);
-    
-    assert msg_to_upper('a')[0] == 'A'
-    #raise AssertionError()
+    test = msg_to_upper('a')[0];
+    print("Test: "+ test);
+    assert msg_to_upper('a')[0] == 'A', 'Noe er fullstendig feil'
     return "Testen er fullført uten feil"
-print test()
-#while 1:
-    # motta data fra client (data, addr)
-    #d = s.recvfrom(1024)
-    #msg = d[0]
-   # addr = d[1]
-     
-    #if not msg: 
-    #    break
-   # print msg
-   # print ' '.join(format(ord(x), 'b') for x in msg) 
-   # reply = ''.join(msg_to_upper(msg))
-    #data.decode('utf-8').upper().encode('utf-8')
-    
-   # print ' '.join(format(ord(x), 'b') for x in reply)
-   # print
-   # s.sendto(reply , addr)
-    #print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.upper()
-while 2:
+print test()    
+#En testfunksjon som bruker assert for å sjekke funksjonen msg_to_upper(),
+#om overføringen fra liten til store bokstaver.
+
+while 1:
     d = s.recvfrom(1024)
+    #mottar data fra client (data, addr). 
     msg = d[0]
     addr = d[1]
-     
-    if not msg: 
+    #Avsenders adresse settes inn en variabel og blir returadressen.
+    if not msg:
         break
     print msg
-    reply = lab2.roman2dec(msg)
-    msg_send = str(reply)
-    s.sendto(msg_send,addr)
-
-     
-
-
-
-
+    print ' '.join(format(ord(x), 'b') for x in msg)
+    reply = ''.join(msg_to_upper(msg))
+    print ' '.join(format(ord(x), 'b') for x in reply)
+    s.sendto(reply , addr)
+    #Går gjennom liste og printer ut bokstavene i lower case som binærtall.
+    
+   
 s.close()
-
